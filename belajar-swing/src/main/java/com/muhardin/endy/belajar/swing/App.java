9fdb6d38d4c37caa47064c3770d8feeb809a3d06
Endy Muhardin <endy.muhardin@gmail.com>
2014-09-26 10:48:49 +0700

    contoh aplikasi swing

---
+++ belajar-swing/src/main/java/com/muhardin/endy/belajar/swing/App.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.swing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
